6ae54bcf6a0185d3f9a97f80d7d28a6ccc705648
Paulo Augusto Hahn <paulo.hahn@hotmail.com>
2023-06-14 20:48:07 +0200

    create client entity

---
+++ README.md
@@ -0,0 +1,5 @@
+Digital Banking
+
+Digital Banking microservice developed together with Digital Innovation One
+
+#Inheritance #Polymorphism #Interface
---
+++ src/com/dio/entities/Client.java
@@ -0,0 +1,19 @@
+package com.dio.entities;
+
+public class Client {
+
+    private String name;
+
+    public Client(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
